678ad2be2ab5b136b1d5c209aec55baeaceac92b
HRD621 <3439144752@qq.com>
2026-04-26 21:03:36 +0800

    fix: CORS header setting in register API

--- api/register.js
+++ api/register.js
@@ -3,14 +3,10 @@ import { logToPostgreSQL } from './_utils/log.js'
 
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
-    return res.status(200).end(
-      '',
-      {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      }
-    )
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    return res.status(200).end('')
   }
 
   if (req.method !== 'POST') {
